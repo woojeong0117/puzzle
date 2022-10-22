67685d0c6d6d35fdb93b66e23e95be8cd68fd3d9
정우정 <112058491+woojeong0117@users.noreply.github.com>
2022-10-22 22:33:22 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,114 @@
+const container = document.querySelector('.image_container');
+const startButton = document.querySelector('.start_button');
+const gameText = document.querySelector('.game_text');
+const playTime = document.querySelector('.play_time');
+
+const tileCount = 16;
+
+let tiles = [];
+const dragged = {
+  el: null,
+  class: null,
+  index: null,
+};
+
+let isPlaying = false;
+let timeInterval = null;
+let time = 0;
+
+// fuctions
+
+function checkStatus() {
+  const currentList = [...container.children];
+  const unMatchedList = currentList.filter((child, index) => Number(child.getAttribute('data-index')) !== index);
+  if (unMatchedList.length === 0) {
+    gameText.style.display = 'block';
+    isPlaying = false;
+    clearInterval(timeInterval);
+  }
+}
+
+function setGame() {
+  isPlaying = true;
+  time = 0;
+  container.innerHTML = '';
+  gameText.style.display = 'none';
+  clearInterval(timeInterval);
+
+  tiles = createImageTiles();
+  tiles.forEach((tile) => container.appendChild(tile));
+  setTimeout(() => {
+    container.innerHTML = '';
+    shuffle(tiles).forEach((tile) => container.appendChild(tile));
+    timeInterval = setInterval(() => {
+      playTime.innerText = time;
+      time++;
+    }, 1000);
+  }, 5000);
+}
+
+function createImageTiles() {
+  const tempArray = [];
+  Array(tileCount)
+    .fill()
+    .forEach((_, i) => {
+      const li = document.createElement('li');
+      li.setAttribute('data-index', i);
+      li.setAttribute('draggable', 'true');
+      li.classList.add(`list${i}`);
+      container.appendChild(li);
+      const img = document.createElement('img');
+      container.appendChild(img);
+      tempArray.push(li);
+    });
+  return tempArray;
+}
+
+function shuffle(array) {
+  // 제일 마지막 인덱스
+  let index = array.length - 1;
+  while (index > 0) {
+    const randomIndex = Math.floor(Math.random() * (index + 1));
+    [array[index], array[randomIndex]] = [array[randomIndex], array[index]];
+    index--;
+  }
+  return array;
+}
+
+// events
+container.addEventListener('dragstart', (e) => {
+  if (!isPlaying) return;
+  const obj = e.target;
+  dragged.el = obj;
+  dragged.class = obj.className;
+  dragged.index = [...obj.parentNode.children].indexOf(obj);
+});
+
+container.addEventListener('dragover', (e) => {
+  e.preventDefault();
+});
+
+container.addEventListener('drop', (e) => {
+  if (!isPlaying) return;
+  const obj = e.target;
+
+  if (obj.className !== dragged.class) {
+    let originPlace;
+    let isLast = false;
+
+    if (dragged.el.nextSibling) {
+      originPlace = dragged.el.nextSibling;
+    } else {
+      originPlace = dragged.el.previousSibling;
+      isLast = true;
+    }
+    const droppedIndex = [...obj.parentNode.children].indexOf(obj);
+    dragged.index > droppedIndex ? obj.before(dragged.el) : obj.after(dragged.el);
+    isLast ? originPlace.after(obj) : originPlace.before(obj);
+  }
+  checkStatus();
+});
+
+startButton.addEventListener('click', () => {
+  setGame();
+});
